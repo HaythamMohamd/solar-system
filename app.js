ac6322ce3841d29b031ca31f2228c854e93d1a82
haytham <haytham.mo7amed@gmail.com>
2024-10-27 16:32:02 +0300

    test

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors())
 //     user: process.env.MONGO_USERNAME,
 //     pass: process.env.MONGO_PASSWORD,
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     // user: 'superuser',
     // pass: 'SuperPassword',
